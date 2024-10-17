dd7a586633893d3b4523f29c5bce50f5b275e0ab
mebharat73 <mebharat73@gmail.com>
2024-10-17 10:22:17 +0545

    Ok now

--- my_project/settings.py
+++ my_project/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+
+from pathlib import Path
 import dj_database_url
 import os
-from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ir)0&nac$$0nprx(&00yp3$9qr-s#d3mst&6))p@%_g0sr^7n%'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG','') != 'False'
 
-ALLOWED_HOSTS = ["*","https://946e-2403-3800-328a-2f96-4833-6ec5-8faf-1ea.ngrok-free.app"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','').split('')
 
 
 # Application definition
