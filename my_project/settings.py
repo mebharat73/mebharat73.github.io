751caf8558e7a34d0ecd943b00f10228d65ea348
mebharat73 <mebharat73@gmail.com>
2024-10-18 17:21:08 +0545

    JJJJ

--- my_project/settings.py
+++ my_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'web',
+    'web.apps.WebConfig',
     'debug_toolbar',
     'django_summernote',
 
