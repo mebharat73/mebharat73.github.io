2f6f9af6226e30a85904f5403631368ae60e91e9
mebharat73 <mebharat73@gmail.com>
2024-10-21 11:52:43 +0545

    hosts

--- my_project/settings.py
+++ my_project/settings.py
@@ -50,7 +50,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("red-csauologph6c73a6k100", 6379)],
         },
     },
 }
