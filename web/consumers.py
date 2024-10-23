45c19ba8ab3f403b9237dae29614f5a6c58b7421
mebharat73 <mebharat73@gmail.com>
2024-10-23 17:40:41 +0545

    websocket

--- web/consumers.py
+++ web/consumers.py
@@ -20,28 +20,32 @@ class ChatConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         username = text_data_json['username']
-        print(f"Received message from {username}: {message}")  # Debugging line
+        profile_pic_url = text_data_json['profile_pic_url']
 
+        # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'username': username
+                'username': username,
+                'profile_pic_url': profile_pic_url,
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
         username = event['username']
-        print(f"Sending message to group: {username}: {message}")  # Debugging line
+        profile_pic_url = event['profile_pic_url']
 
+        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
-            'username': username
+            'username': username,
+            'profile_pic_url': profile_pic_url,
         }))
-        
