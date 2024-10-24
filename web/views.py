a4832dbac8cfb6babf8692be577932d5e8a73912
mebharat73 <mebharat73@gmail.com>
2024-10-24 18:18:29 +0545

    ABACHAHe

--- web/views.py
+++ web/views.py
@@ -66,7 +66,7 @@ def room(request, room_name):
     room = Room.objects.get(name=room_name)
     messages = Message.objects.filter(room=room).order_by('timestamp')
 
-    return render(request, 'room.html', {'room': room, 'messages': messages})
+    return render(request, 'chat/room.html', {'room': room, 'messages': messages})
 
 def index(request):
     return render(request, "main/index.html")
