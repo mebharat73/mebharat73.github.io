6f0735e254e95a30bc475e8dcd1752becec0f523
mebharat73 <mebharat73@gmail.com>
2024-10-26 13:20:11 +0545

    HHHHHHH

--- my_project/asgi.py
+++ my_project/asgi.py
@@ -2,10 +2,12 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import web.routing  # Replace with your actual app's routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_project.settings')  # Replace with your project name
+application = get_asgi_application()
 
+
+import web.routing  # Replace with your actual app's routing
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
