6aa624d0ebfa14db4136462b63e1cea01ea77a96
mebharat73 <mebharat73@gmail.com>
2024-12-03 11:25:16 +0545

    Your commit message describing the changes

--- my_project/settings.py
+++ my_project/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'defaultdb',  # Your database name
         'USER': 'avnadmin',    # Your username
-        'PASSWORD': os.getenv('AIVEN_SERVICE_PASSWORD'),  # Ensure this is not hardcoded
+        'PASSWORD': os.getenv('AIVEN_SERVICE_PASSWORD'),  # Load from environment variable
         'HOST': 'bharatkarki-mebharat73-4e98.e.aivencloud.com',  # Your host
         'PORT': '23616',  # Your port
         'OPTIONS': {
