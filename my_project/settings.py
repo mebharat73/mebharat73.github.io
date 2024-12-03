7f847220373e2678a65d56c191247f17b23b2cfb
mebharat73 <mebharat73@gmail.com>
2024-12-03 19:24:25 +0545

    lalala

--- my_project/settings.py
+++ my_project/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ir)0&nac$$0nprx(&00yp3$9qr-s#d3mst&6))p@%_g0sr^7n%'
-
+#SECRET_KEY = 'django-insecure-ir)0&nac$$0nprx(&00yp3$9qr-s#d3mst&6))p@%_g0sr^7n%'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") != "False"
 
 ALLOWED_HOSTS = ['*']
 
@@ -107,7 +107,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'my_project.wsgi.application'
+#WSGI_APPLICATION = 'my_project.wsgi.application'
 
 
 
--- my_project/urls.py
+++ my_project/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("web.urls")),
     path('summernote/', include('django_summernote.urls')),  
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
